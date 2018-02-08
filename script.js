a8aca37ff61b998cd3209f84f593f2f9f8c914ec
Karolina <kika.romanska@gmail.com>
2018-02-08 17:29:39 +0100

    Score added

--- script.js
+++ script.js
@@ -1,48 +1,43 @@
+
 const holes = document.querySelectorAll('.hole');
-const scoreBord = document.querySelector('.score');
-const moles = document.querySelectorAll('.moles');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
 let lastHole;
 let timeUp = false;
 let score = 0;
-
 function randomTime(min, max) { // fn that gives us random amount of time between hover min and max
-    return Math.round(Math.random() * (max - min) + min)
+  return Math.round(Math.random() * (max - min) + min);
 }
-
 function randomHole(holes) { // fn that's going to pick a random hole fot the mole to pop up
-    const idx = Math.floor(Math.random() * holes.length);
-    const hole = holes[idx];
-    if(hole === lastHole) {
-        console.log('Ah nah thats the same hole bud!');
-        return randomHole(holes);
-    }
-    lastHole = hole;// save the reference to what one got popped last time
-    return hole;
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    console.log('Ah nah thats the same one bud');
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
 }
-
 function peep() {
-    const time = randomTime(200, 1000);
-    const hole = randomHole(holes);
-    hole.classList.add('up');
-    setTimeout(() => {
-        hole.classList.remove('up');
-        if(!timeUp) peep();
-    }, time);
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if (!timeUp) peep();
+  }, time);
 }
-
 function startGame() {
-    scoreBord.textContent = 0;
-    timeUp = false;
-    score = 0;
-    peep();
-    setTimeout(() => timeUp = true, 10000);
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => timeUp = true, 10000)
 }
-
 function bonk(e) {
-    // if(!e.isTrusted) return; // cheater!
-    // score++;
-    // this.parentNode.classList.remove('up');
-    // scoreBord.textContent = score;
-    console.log('Got you!')
+  if(!e.isTrusted) return; // cheater!
+  score++;
+  this.parentNode.classList.remove('up');
+  scoreBoard.textContent = score;
 }
 moles.forEach(mole => mole.addEventListener('click', bonk));
